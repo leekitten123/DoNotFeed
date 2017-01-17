c3ae7836e193be3d3a81b78f6770af364be5372a
LeeSangWook <leekitten123@hanmail.net>
2017-01-17 19:53:16 +0900

    Shoot Nose

---
+++ DoNotFeed/Assets/BulletMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+    int moveSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(moveSpeed * Time.deltaTime, 0, 0);	
+	}
+}
---
+++ DoNotFeed/Assets/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    GameObject pig;
+
+	// Use this for initialization
+	void Start () {
+        pig = GameObject.Find("Pig");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(pig.transform.position.x + 30, pig.transform.position.y + 25, pig.transform.position.z - 4);
+	}
+}
