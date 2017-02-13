ec176791c58e452365412f1b37bca38353b2a429
LeeSangWook <leekitten123@hanmail.net>
2017-02-13 23:11:20 +0900

    Make JoyStick

---
+++ DoNotFeed/Assets/Scripts/StartManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //** 안드로이드 일 경우 뒤로가기 버튼 클릭시 어플리케이션 종료한다??? **/
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    /** Start 버튼 클릭 시 게임을 시작한다. **/
+    public void StartGame()
+    {
+        Application.LoadLevel("Stage1");   
+    }
+}
